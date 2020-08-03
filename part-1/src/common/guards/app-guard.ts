5fef7255ed780e0dd172c439434d5bcac86b524f
montsouz <guilhermesouzamont@gmail.com>
2020-08-02 23:02:14 -0300

    fix:part-1:guards dont accept empty bodys anymore

--- part-1/src/common/guards/app-guard.ts
+++ part-1/src/common/guards/app-guard.ts
@@ -11,13 +11,17 @@ export class AppGuard {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const message = context.getArgByIndex(0);
-    const hashedMessage = hash(message.body);
-    if (await this.redisClient.get(hashedMessage)) {
-      Logger.log('You shall not pass!', 'Guard', false);
-      return false;
+    if (message.body && Object.keys(message.body).length !== 0) {
+      const hashedMessage = hash(message.body);
+      if (await this.redisClient.get(hashedMessage)) {
+        Logger.log('You shall not pass!', 'Guard', false);
+        return false;
+      }
+      this.redisClient.set(hashedMessage, 'true', 'ex', 10 * 60);
+      Logger.log('You are free to go :)', 'Guard', false);
+      return true;
     }
-    this.redisClient.set(hashedMessage, 'true', 'ex', 10 * 60);
-    Logger.log('You are free to go :)', 'Guard', false);
-    return true;
+    Logger.log('Dont try to send me NULL!', 'Guard', false);
+    return false;
   }
 }
